9c240288272c9f54776495674d7d6ea9d6355e2f
elAsksito <xpheri035@gmail.com>
2025-07-14 22:41:09 -0500

    Update Main

---
+++ Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Hello World!
+    cout << "Hello World!"  << endl;
+
+    // Variables
+
+    string name;
+    int age;
+    double salary;
+
+    cout << "Por favor ingresa tu nombre:" << endl;
+    getline(cin >> ws, name);
+
+    cout << "Por favor ingresa tu edad:" << endl;
+    cin >> age;
+
+    cout << "Por favor ingresa tu sueldo:" << endl;
+    cin >> salary;
+
+    cout << "Hola " << name << endl;
+
+    if (age <= 17) {
+        cout << "Tienes " << age << " eres menor de edad" << endl;
+    } else {
+        cout << "Tienes " << age << " eres mayor de edad" << endl;
+    }
+
+    if (salary <= 1500) {
+        cout << "Con tu sueldo " << salary << " eres un Junior" << endl;
+    } else if (salary <= 4500) {
+        cout << "Con tu sueldo " << salary << " eres un Semi Senior" << endl;
+    } else {
+        cout << "Con tu sueldo " << salary << " eres un Senior" << endl;
+    }
+
+    return 0;
+}
